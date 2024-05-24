38708f70c06c7e7a9702ffac34c6cb9ad1e766b7
rotem nevgauker <rotemnevgauker@rotems-MacBook-Pro.local>
2024-05-25 00:26:43 +0900

    updates

--- app/form_generator/index.tsx
+++ app/form_generator/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Dialog,
   DialogContent,
@@ -13,9 +13,13 @@ import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 
 import { useFormState, useFormStatus } from 'react-dom'
-import { generateForm } from '@/app/actions/generateForm'
+
 import { useSession, signIn } from 'next-auth/react'
-import { navigate } from '@/app/actions/navigateToForm'
+import { navigate } from '../actions/navigateToForm'
+
+import { Plus } from 'lucide-react'
+import { usePlausible } from 'next-plausible'
+import { generateForm } from '@/app/actions/generateForm'
 
 type Props = {}
 
@@ -34,20 +38,12 @@ export function SubmitButton() {
     </Button>
   )
 }
+
 const FormGenerator = (props: Props) => {
   const [state, formAction] = useFormState(generateForm, initialState)
   const [open, setOpen] = useState(false)
   const session = useSession()
-
-  // form for developer to understand what  tech stack they want to leran and what are their career goals
-
-  const onFromCreate = () => {
-    if (session.data?.user) {
-      setOpen(true)
-    } else {
-      signIn()
-    }
-  }
+  const plausible = usePlausible()
 
   useEffect(() => {
     if (state.message === 'success') {
@@ -55,12 +51,25 @@ const FormGenerator = (props: Props) => {
       navigate(state.data.formId)
     }
   }, [state.message])
+
+  const onFormCreate = () => {
+    plausible('create-form')
+    if (session.data?.user) {
+      setOpen(true)
+    } else {
+      signIn()
+    }
+  }
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <Button onClick={onFromCreate}>Create Form</Button>
+      <Button onClick={onFormCreate}>
+        <Plus className='w-4 h-4 mr-2' />
+        Create Form
+      </Button>
       <DialogContent className='sm:max-w-[425px]'>
         <DialogHeader>
-          <DialogTitle>Create a new form</DialogTitle>
+          <DialogTitle>Create New Form</DialogTitle>
         </DialogHeader>
         <form action={formAction}>
           <div className='grid gap-4 py-4'>
@@ -68,7 +77,7 @@ const FormGenerator = (props: Props) => {
               id='description'
               name='description'
               required
-              placeholder='Share what your from is about, who is it for, and what information you would like to collect. And AI will do the magic'
+              placeholder='Share what your form is about, who is it for, and what information you would like to collect. And AI will do the magic ✨'
             />
           </div>
           <DialogFooter>
